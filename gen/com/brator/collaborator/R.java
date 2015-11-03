952168f71fa8d1e617ddc6ddd81f93d8abc7eaa8
unknown <>
2015-11-03 16:40:41 +0800

    Signed-off-by: KNCX <王俊钦>

--- gen/com/brator/collaborator/R.java
+++ gen/com/brator/collaborator/R.java
@@ -28,53 +28,7 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int action_settings=0x7f08002b;
-        public static final int address=0x7f080009;
-        public static final int asker=0x7f080015;
-        public static final int btnChangePortrait=0x7f08000e;
-        public static final int btnSetLikedQuestionType=0x7f08000f;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int confirmPasswordEditText=0x7f080028;
-        public static final int confirmPasswordFrame=0x7f080026;
-        public static final int confirmPasswordTextView=0x7f080027;
-        public static final int editText1=0x7f08001c;
-        public static final int editText2=0x7f08001e;
-        public static final int hint_address=0x7f080008;
-        public static final int hint_asker=0x7f080014;
-        public static final int hint_numof_ame_question=0x7f080012;
-        public static final int hint_question_content=0x7f080006;
-        public static final int hint_question_title=0x7f080004;
-        public static final int hint_reward=0x7f080019;
-        public static final int hint_status=0x7f080017;
-        public static final int hint_variety=0x7f080010;
-        public static final int numofsamequestion=0x7f080013;
-        public static final int parentFrame=0x7f08001f;
-        public static final int pwdEditText=0x7f080025;
-        public static final int pwdFrame=0x7f080023;
-        public static final int pwdTextView=0x7f080024;
-        public static final int questionTagSelector=0x7f080002;
-        public static final int question_content=0x7f080007;
-        public static final int question_title=0x7f080005;
-        public static final int registrationResult=0x7f08000c;
-        public static final int registrationSubmit=0x7f08002a;
-        public static final int result=0x7f080029;
-        public static final int reward=0x7f08000a;
-        public static final int settings=0x7f080003;
-        public static final int status=0x7f080018;
-        public static final int submit=0x7f08000b;
-        public static final int take_order=0x7f08001a;
-        public static final int textView1=0x7f08001b;
-        public static final int textView2=0x7f08001d;
-        public static final int time=0x7f080016;
-        public static final int tnChangePassword=0x7f08000d;
-        public static final int userNameEditText=0x7f080022;
-        public static final int userNameFrame=0x7f080020;
-        public static final int userNameTextView=0x7f080021;
-        public static final int variety=0x7f080011;
-=======
-        public static final int action_settings=0x7f08003b;
+        public static final int action_settings=0x7f08003f;
         public static final int addQuestionIcon=0x7f080002;
         public static final int address=0x7f08002b;
         public static final int asker=0x7f080027;
@@ -82,10 +36,12 @@ public final class R {
         public static final int btnSetLikedQuestionType=0x7f080021;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
-        public static final int confirmPasswordEditText=0x7f080038;
-        public static final int confirmPasswordFrame=0x7f080036;
-        public static final int confirmPasswordTextView=0x7f080037;
+        public static final int confirmPasswordEditText=0x7f08003c;
+        public static final int confirmPasswordFrame=0x7f08003a;
+        public static final int confirmPasswordTextView=0x7f08003b;
         public static final int day=0x7f080017;
+        public static final int editText1=0x7f080030;
+        public static final int editText2=0x7f080032;
         public static final int hint_address=0x7f08002a;
         public static final int hint_asker=0x7f080026;
         public static final int hint_location=0x7f080013;
@@ -109,10 +65,10 @@ public final class R {
         public static final int location=0x7f080014;
         public static final int month=0x7f080016;
         public static final int numofsamequestion=0x7f080025;
-        public static final int parentFrame=0x7f08002f;
-        public static final int pwdEditText=0x7f080035;
-        public static final int pwdFrame=0x7f080033;
-        public static final int pwdTextView=0x7f080034;
+        public static final int parentFrame=0x7f080033;
+        public static final int pwdEditText=0x7f080039;
+        public static final int pwdFrame=0x7f080037;
+        public static final int pwdTextView=0x7f080038;
         public static final int questionSubmitPage=0x7f08000e;
         public static final int questionTagSelector=0x7f080003;
         public static final int question_content=0x7f080012;
@@ -120,21 +76,22 @@ public final class R {
         public static final int question_type=0x7f08001c;
         public static final int refund=0x7f080019;
         public static final int registrationResult=0x7f08001e;
-        public static final int registrationSubmit=0x7f08003a;
-        public static final int result=0x7f080039;
+        public static final int registrationSubmit=0x7f08003e;
+        public static final int result=0x7f08003d;
         public static final int reward=0x7f08002e;
         public static final int settings=0x7f080004;
         public static final int status=0x7f080029;
         public static final int submit=0x7f08001d;
         public static final int take_order=0x7f08002d;
         public static final int testButton=0x7f080005;
+        public static final int textView1=0x7f08002f;
+        public static final int textView2=0x7f080031;
         public static final int time=0x7f080018;
         public static final int tnChangePassword=0x7f08001f;
-        public static final int userNameEditText=0x7f080032;
-        public static final int userNameFrame=0x7f080030;
-        public static final int userNameTextView=0x7f080031;
+        public static final int userNameEditText=0x7f080036;
+        public static final int userNameFrame=0x7f080034;
+        public static final int userNameTextView=0x7f080035;
         public static final int variety=0x7f080023;
->>>>>>> 1d10d520c91908e015ca792ae31aa384596e868b
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -142,22 +99,14 @@ public final class R {
         public static final int evaluate_answerer=0x7f030002;
         public static final int my_questions=0x7f030003;
         public static final int question_home=0x7f030004;
-<<<<<<< HEAD
-        public static final int question_submit=0x7f030005;
-        public static final int questions_list=0x7f030006;
-        public static final int register_result=0x7f030007;
-        public static final int settings_home=0x7f030008;
-        public static final int single_question=0x7f030009;
-        public static final int user_login=0x7f03000a;
-        public static final int user_register=0x7f03000b;
-=======
         public static final int question_home_item=0x7f030005;
         public static final int question_submit=0x7f030006;
-        public static final int register_result=0x7f030007;
-        public static final int settings_home=0x7f030008;
-        public static final int single_question=0x7f030009;
-        public static final int user_register=0x7f03000a;
->>>>>>> 1d10d520c91908e015ca792ae31aa384596e868b
+        public static final int questions_list=0x7f030007;
+        public static final int register_result=0x7f030008;
+        public static final int settings_home=0x7f030009;
+        public static final int single_question=0x7f03000a;
+        public static final int user_login=0x7f03000b;
+        public static final int user_register=0x7f03000c;
     }
     public static final class menu {
         public static final int main=0x7f070000;
