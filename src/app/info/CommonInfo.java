fa13532c08a259cb3e9e1f9b4477faf12c26cf26
unknown <>
2015-11-27 21:19:42 +0800

    KN's New Android Studio Branch
    Signed-off-by: kncxstudio <王俊钦>

--- src/app/info/CommonInfo.java
+++
@@ -1,8 +0,0 @@
-package app.info;
-
-public class CommonInfo {
-	public static String registerUrl = "http://dev.iboxshare.com/askapp/register/register.php"; //注册
-	public static String addFundUrl = "http://dev.iboxshare.com/askapp/test/payment/addFund.php";//唯一充值订单码
-	public static String questionSubmitUrl = "";//Post，提交问题
-	public static String questionShowUrl = "";//Get，问题圈中得到问题用以展览
-}
--- src/com/brator/collaborator/AddFund.java
+++
@@ -1,7 +0,0 @@
-package com.brator.collaborator;
-
-import android.app.Activity;
-
-public class AddFund extends Activity {
-
-}
